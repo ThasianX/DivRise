2606b420f8e4587406b5393f13b9974d1447d5f2
Kevin Li <kevinli29033@gmail.com>
2019-12-23 17:12:49 -0800

    Initial Commit

---
+++ Dividend App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dividend App
+//
+//  Created by Kevin Li on 12/23/19.
+//  Copyright © 2019 Kevin Li. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
