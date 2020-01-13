7c54fc5a576cd6788e71cc328d46c17a0f315573
kevin.li <kevin.li@duethealth.com>
2020-01-13 14:10:44 -0500

    config file

--- Dividend App/Networking/Configuration.swift
+++ Dividend App/Networking/Configuration.swift
@@ -9,7 +9,7 @@
 import Foundation
 
 struct Configuration {
-    let alphaVantageApiKey = "NL32VMEBCYJL7P86"
-    let newsApiKey = "0fef0c5bfc4f4936b19f8cf5dde9fb1e"
-    let iexApiKey = "Tsk_654a34bcb8534b22934ed38896ce3d61"
+    let alphaVantageApiKey = ""
+    let newsApiKey = ""
+    let iexApiKey = ""
 }
